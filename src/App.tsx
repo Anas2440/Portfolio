8c2010031856024c627cdf9fed88a56793fc9e18
Anas Parekh <anasparekh5@gmail.com>
2025-06-12 08:15:26 +0530

    image changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './contexts/ThemeContext';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import FloatingWidget from './components/FloatingWidget';
-import Home from './pages/Home';
-import Portfolio from './pages/Portfolio';
-import ProjectDetail from './pages/ProjectDetail';
-import About from './pages/About';
-import Services from './pages/Services';
-import Pricing from './pages/Pricing';
-import Testimonials from './pages/Testimonials';
-import Contact from './pages/Contact';
-import Blog from './pages/Blog';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import FloatingWidget from "./components/FloatingWidget";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import ProjectDetail from "./pages/ProjectDetail";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Pricing from "./pages/Pricing";
+import Testimonials from "./pages/Testimonials";
+import Contact from "./pages/Contact";
+import Blog from "./pages/Blog";
 
 function App() {
   return (
